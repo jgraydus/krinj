079a939a4736284a90c949d857d0c7edf7995351
josh <jgraydus@gmail.com>
2022-08-25 08:54:14 -0600

    some work on issues page

--- client/src/issues/index.tsx
+++ client/src/issues/index.tsx
@@ -1,8 +1,58 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import api from '../api'
+import Loading from '../components/loading'
+import Table from './table'
 
 const Root = styled.div`
-  border: 1px solid purple;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column nowrap;
 `
+const Header = styled.div`
+  font-size: 20px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+`
+const NoIssues = styled.div`
+  width: 100%;
+  height: 100%;
+  font-size: 30px;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: center;
+`
+const Content = ({ issues }) => {
+  if (issues == null) {
+      return <Loading />;
+  }
+  if (issues.length === 0) {
+      return <NoIssues>No Issues</NoIssues>;
+  }
+  return <Table issues={issues} />;
+}
 
-export default () =>
-  <Root>Issues</Root>
+export default ({ projectId }) => {
+  const [issues, setIssues] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+        const issues = await api.getApiV1Issues();
+        setIssues(issues);
+    })()
+  }, [projectId]);
+
+  return (
+    <Root>
+      <Header>Issues</Header>
+      <Content issues={issues} />
+    </Root>
+  )
+}
---
+++ client/src/issues/table.tsx
@@ -0,0 +1,7 @@
+
+export default ({ issues }) => {
+
+  return (
+    <div>TODO ISSUES</div>
+  )
+}
