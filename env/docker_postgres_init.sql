82e731213dbe0620196eb5177790ad0c3cf1c713
josh <jgraydus@gmail.com>
2022-09-19 10:10:50 -0600

    docker config for postgres

---
+++ env/docker_postgres_init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE main
+    WITH
+    OWNER = postgres 
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION_LIMIT = -1;
+
