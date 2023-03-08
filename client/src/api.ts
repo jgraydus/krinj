475e84d92fcf3db93651eb69cab19b45a94af6e3
josh <jgraydus@gmail.com>
2023-03-07 19:10:37 -0700

    fix api. pull data off result

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,19 +2,19 @@ import * as R from 'ramda'
 import axios from 'axios'
 
 const getProjects = (): Promise<Array<Project>> =>
-    axios.get('/api/projects')
+    axios.get('/api/projects').then(x => x.data);
 
 const getProject = (projectId: ProjectId): Promise<Project> =>
-    axios.get(`/api/projects/${projectId}`)
+    axios.get(`/api/projects/${projectId}`).then(x => x.data);
 
 const createProject = (arg: { projectName: string, projectDescription: string }): Promise<Project> =>
-    axios.post(`/api/projects`, arg)
+    axios.post(`/api/projects`, arg).then(x => x.data);
 
 const updateProject = (projectId: ProjectId, arg: { projectName: string, projectDescription: string }): Promise<Project> =>
-    axios.patch(`/api/projects/${projectId}`, arg)
+    axios.patch(`/api/projects/${projectId}`, arg).then(x => x.data);
 
 const deleteProject = (projectId: string): Promise<void> =>
-    axios.delete(`/api/projects/${projectId}`)
+    axios.delete(`/api/projects/${projectId}`).then(x => x.data);
 
 export default {
     getProjects, getProject, createProject, updateProject, deleteProject
